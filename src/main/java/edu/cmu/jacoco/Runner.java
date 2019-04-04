f2dd43e70a6c9aa588127773f686b612acbd147e
Alexey Emelin <dolf@yandex-team.ru>
2019-04-04 07:55:38 +0300

    fix teamcity spaces

--- src/main/java/edu/cmu/jacoco/Runner.java
+++ src/main/java/edu/cmu/jacoco/Runner.java
@@ -20,10 +20,51 @@ import static java.util.concurrent.Executors.newFixedThreadPool;
 public class Runner {
 
     public static void main(final String[] args) throws ParseException, IOException, ExecutionException, InterruptedException {
+        List<String> fixedArgs = new ArrayList<String>() {
+            @Override
+            public boolean add(String o) {
+                return super.add(o.replace("\"", ""));
+            }
+
+            @Override
+            public String set(int index, String element) {
+                return super.set(index, element.replace("\"", ""));
+            }
+        };
+
+        boolean listStarted = false;
+        for (String arg: args) {
+            System.out.println("[Jacoco comparison tool] Args: " + arg);
+
+            if (arg.startsWith("\"") && arg.endsWith("\"")) {
+                fixedArgs.add(arg);
+                continue;
+            }
+
+            if (arg.startsWith("\"")) {
+                listStarted = true;
+                fixedArgs.add(arg);
+                continue;
+            }
+
+            if (!listStarted) {
+                fixedArgs.add(arg);
+                continue;
+            }
+
+            if (arg.endsWith("\"")) {
+                listStarted = false;
+            }
+
+            int elements = fixedArgs.size() - 1;
+            String lastArg = fixedArgs.get(elements);
+            fixedArgs.set(elements, lastArg + arg);
+        }
+
         System.out.println("[Jacoco comparison tool] Start: " + new Date().toString());
 
         ArgumentsExtractor argumentsExtractor = new ArgumentsExtractor();
-        ArgumentsExtractor.Arguments arguments = argumentsExtractor.extractArguments(args);
+        ArgumentsExtractor.Arguments arguments = argumentsExtractor.extractArguments((String[]) fixedArgs.toArray());
 
         List<IBundleCoverage> coverages = analyze(
                 arguments.first.stream().map(File::new).collect(Collectors.toList()),
